841fbc3916099db7c7a821ec07503a9e2173d672
Perera Suarez-external <pedro-rafael.perera-suarez-external@capgemini.com>
2020-03-25 14:55:35 +0100

    img ok

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 
 import "./App.css";
 import Board from "./componets/Board";
+import { DndProvider } from "react-dnd";
+import Backend from "react-dnd-html5-backend";
 
 function App() {
   return (
     <div className="App">
-      <Board />
+      <DndProvider backend={Backend}>
+        <Board />
+      </DndProvider>
     </div>
   );
 }
--- src/componets/Board.js
+++ src/componets/Board.js
@@ -1,14 +1,38 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { useDrag, useDrop } from "react-dnd";
 
 const Board = () => {
   const [buttonState, setButtonState] = useState(false);
+  const [currentIMG, setCurrentImg] = useState("");
+  const [srcImg, setSrcImg] = useState("");
+  var src = "./img/" + srcImg;
 
-  const img = useRef("");
+  const ItemTypes = {
+    KNIGHT: "knight"
+  };
+
+  const [collectedProps, drop] = useDrop({
+    accept
+  });
+
+  const [{ isDragging }, drag] = useDrag({
+    item: { type: ItemTypes.KNIGHT },
+    collect: monitor => ({
+      isDragging: !!monitor.isDragging()
+    })
+  });
+
+  useEffect(() => {
+    src = "./img/" + srcImg;
+  }, [srcImg]);
 
   const saveIMG = () => {
     setButtonState(false);
+    if (currentIMG != " " || currentIMG != undefined) {
+      let string = currentIMG.split("\\");
 
-    console.log(img.current.value);
+      setSrcImg(string[string.length - 1]);
+    }
   };
   const editIMG = () => {
     setButtonState(true);
@@ -16,12 +40,18 @@ const Board = () => {
 
   return (
     <>
-      <div className="card">
+      <div className="card" ref={drop}>
         <div className="card-image">
           {!buttonState ? (
-            <img src={img.current.value}></img>
+            <img ref={drag} src={src}></img>
           ) : (
-            <input type="file" ref={img} name="img"></input>
+            <input
+              type="file"
+              name="img"
+              onChange={e => {
+                setCurrentImg(e.target.value);
+              }}
+            ></input>
           )}
         </div>
         <a className="btn-floating halfway-fab waves-effect waves-light red flotar">
